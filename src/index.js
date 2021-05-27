f9b4cdafb095d0d84f1929e7d90a6d663e6ceb05
Joshua Burger <joshberger6969@outlook.com>
2021-05-26 23:25:50 -0400

    new variable gamesContainer to grab the element by id from games-container on index.html, itterate over the data displaying each game with attributes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 function fetchGames() {
+    const gamesContainer = document.getElementById("games-container")
+
     fetch("http://localhost:3000/api/v1/games")
     .then(r => r.json())
-    .then(data => console.log(data) )
+    .then(data => {
+        data.forEach(function(game){
+            gamesContainer.innerHTML += `<li>Game: ${game.title}<br>Price: $${game.price}<br>Genre: ${game.genre}<br>Platform: ${game.platform}</li><br>`
+        })
+    })
     .catch(err => console.log(err))
 }
