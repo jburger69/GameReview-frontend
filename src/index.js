211aef39ab126d7fa9b089eb9d088839aba55369
Joshua Burger <joshberger6969@outlook.com>
2021-05-28 12:10:09 -0400

    Refactor fetchgames() took everything and transfered to game class and instansitate a new game and add to dom

--- src/index.js
+++ src/index.js
@@ -9,17 +9,9 @@ function fetchGames() {
     fetch("http://localhost:3000/api/v1/games")
     .then(r => r.json())
     .then(data => {
-        data.forEach(function(game){
-            gamesContainer.innerHTML += `<div class="card">
-            <div class="card-body">
-            <h4 class="card-title">Game: ${game.title}<br></h4>
-            <p class="card-text">
-            Price: $${game.price}<br>
-            Genre: ${game.genre}<br>
-            Platform: ${game.platform}<br>
-            </p>
-            </div>
-            </div>`
+        data.forEach(game => {
+            const g = new Game(game)
+            g.addToDom()
         })
     })
     .catch(err => console.log(err))
