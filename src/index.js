429638a82c59497aaadb07d39aefcccfa3e732d8
Joshua Burger <joshberger6969@outlook.com>
2021-06-10 16:15:12 -0400

    Remove comment, Change fetch URL to localhost:3000/api/v1/games/id

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 function fetchGames() {
-    const gamesContainer = document.getElementById("games-container")
-
     fetch("http://localhost:3000/api/v1/games")
     .then(r => r.json())
     .then(data => {
@@ -27,23 +25,18 @@ function fetchGames() {
 const mainDiv = document.getElementById("games-container")
 mainDiv.addEventListener("click", (event) => {
     e = event.target
+    id = event.target.id
     switch(event.target.dataset.id){
         case "reviews":
-            fetch(gameURL)
+            fetch(`http://localhost:3000/api/v1/games/${id}`)
             .then(r => r.json())
             .then(data => {
-                Game.getInfo(data.find(g => g.id == e.id))
+                Game.getInfo(data)
             })
             .catch(err => console.log(err))
 
             }
     })
-
-
-// Game.all.forEach(game => {
-//     const g = new Game(game)
-//     g.addToDom()
-// })
 
 
 
